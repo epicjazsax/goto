eb1b59f09c97b687c75e5da14a035e92055ba9b6
Justin Zakrajsek <justin.zakrajsek@gmail.com>
2025-11-19 10:25:20 -0800

    Begin format change to search and results

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import '/node_modules/tachyons/css/tachyons.css'
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld/>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
