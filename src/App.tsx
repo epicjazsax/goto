c6a14b750e74068e9c62c6d2e262518d9872b415
Tom Zakrajsek <tzakrajsek@qdusa.com>
2026-01-19 23:08:48 -0800

    Experimental changes... validate

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import qdLogo_dark from './assets/qd_logo_main_dark.png'
 // import qdLogo1 from './assets/qd-logo1.png'
 // import qdLogo3 from './assets/qd-logo3.png'
 // import qdLogoGif1 from './assets/logo_anim_play_once.gif'
-
+import {ReleasePackage} from "./components/Packages.ts";
 
 import CardList from './components/CardList'
 import ErrorBoundary from './components/ErrorBoundary'
@@ -22,6 +22,27 @@ function App() {
             .then(res => res.json())
             .then(data => {
                 setEntries(data)
+
+                try {
+                    // Read and parse the file
+                    // const data = JSON.parse(fileContent);
+
+                    // Validate that it's an array of strings
+                    if (!Array.isArray(data)) {
+                        throw new Error('JSON content must be an array');
+                    }
+
+                    // Map each string to a new instance of the provided class
+                    return data.map((item: unknown) => {
+                        // if (typeof item !== 'string') {
+                        //     throw new Error('All items in the array must be strings');
+                        // }
+                        return new ReleasePackage(item);
+                    });
+                } catch (err) {
+                    const message = err instanceof Error ? err.message : String(err);
+                    throw new Error(`Failed to load or process JSON data": ${message}`);
+                }
             })
     }, [])
 
@@ -29,23 +50,20 @@ function App() {
         setSearchField(event.target.value);
     }
 
+
+    // ReleasePackages =0;
+    //
+    // const jsonString = '{"package_name": "VersaLab", "location": "Trantor", "maintainer": "tzakrajsek"}';
+    // const pkg: ReleasePackage = new ReleasePackage(JSON.parse(jsonString));
+    // console.log(pkg);
+
+
     const filteredEntries = filterEntriesByString(entries, searchField)
 
     return !entries.length ?
         <div>Loading...</div> :
         (
             <>
-                {/*
-                <div className="logo-container">
-                    <a href="https://qdusa.com/" target="_blank">
-                        <img src={qdLogoGif1} className="logo" alt="Quantum Design" />
-                    </a>
-                    <a href="https://qdusa.com/" target="_blank">
-                        <img src={cryoLogo} className="logo react" alt="Cryo" />
-                    </a>
-                </div>
-                */}
-
                 <div className="logo-container">
                     <a href="https://qdusa.com/" target="_blank">
                         <img src={qdLogo_dark} className="logo" alt="Quantum Design" />
