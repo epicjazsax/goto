6e97f91c20f1ef457785ad0259b438c66e716583
Tom Zakrajsek <tzakrajsek@qdusa.com>
2026-01-19 19:42:39 -0800

    Clean up warnings

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,8 @@
-const Card = ({ package_name, location, maintainer }: { package_name: string; location: string; maintainer: string }) => {
+const Card = ({ package_name, maintainer }: { package_name: string; maintainer: string }) => {
     return (
         <div className='.card bg-light-green dib br3 pa3 ma2 shadow-hover bw2 black' data-testid='card'>
             <div>
                 <h2>{package_name}</h2>
-                {/* <h4>{location}</h4> */}
                 {maintainer && <p>Maintainer: {maintainer}</p>}
             </div>
         </div>
--- src/components/FilteredEntries.ts
+++ src/components/FilteredEntries.ts
@@ -2,9 +2,9 @@ export function filterEntriesByString(entries: { package_name: string; location:
     // console.log(`Filtering ${entries.length} entries with search term: ${searchField}`);
     return entries.filter(entry => {
         // console.log(`Checking entry: ${JSON.stringify(entry)}`);
-        return (typeof entry.package_name !== 'undefined' && entry.package_name.toLowerCase().includes(searchField.toLowerCase()))
-            || (typeof entry.location !== 'undefined' && entry.location.toLowerCase().includes(searchField.toLowerCase()))
-            || (typeof entry.maintainer !== 'undefined' && entry.maintainer.toLowerCase().includes(searchField.toLowerCase()))
+        return (entry.package_name !== undefined && entry.package_name.toLowerCase().includes(searchField.toLowerCase()))
+            || (entry.location !== undefined && entry.location.toLowerCase().includes(searchField.toLowerCase()))
+            || (entry.maintainer !== undefined && entry.maintainer.toLowerCase().includes(searchField.toLowerCase()))
     })
 }
 
