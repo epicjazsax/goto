46e4d9c6f2bcc59a47246b1a500aecb63ce9fb9c
Justin Zakrajsek <justin.zakrajsek@gmail.com>
2026-01-28 14:07:36 -0800

    Update title and icon position

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <div>Loading...</div> :
         (
             <>
-                <div className='bg-blue-800 border border-white inline-flex'>
+                <div className='fixed top-0 left-0 bg-blue-800 border border-white flex'>
                     <a href="https://vite.dev" target="_blank">
                         <img src={viteLogo} className="logo" alt="Vite logo" />
                     </a>
@@ -38,12 +38,12 @@ function App() {
                         <img src={reactLogo} className="logo react" alt="React logo" />
                     </a>
                 </div>
-                <h1 className='bg-yellow-800 border border-white my-4 py-4'>
+                <h1 className='relative left-1/2 transform -translate-x-1/2 w-3/4 bg-yellow-800 border border-white my-4 py-4'>
                     Go-To Manager
                 </h1>
                 <h3 className='border border-white m-3 p-3'>
                     <SearchBox searchChange={onSearchFieldChange} />
-                    <div className='bg-red-800 border border-white m-1 p-1'>
+                    <div className='bg-red-800 border border-white m-1 p-1 inline-flex'>
                         Search Term is: {searchField}
                     </div>
                 </h3>
